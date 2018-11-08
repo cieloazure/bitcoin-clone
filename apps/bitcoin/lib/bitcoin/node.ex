c5971a7d6f5018591c7333010ab367795157bce8
Akash S <akash.shingte@gmail.com>
2018-11-07 22:18:45 -0500

    Documentation for node

--- apps/bitcoin/lib/bitcoin/node.ex
+++ apps/bitcoin/lib/bitcoin/node.ex
@@ -1,14 +1,45 @@
 defmodule Bitcoin.Node do
+  @moduledoc """
+  A Bitcoin full node
+  """
   use GenServer
 
+  ###             ###
+  ###             ###
+  ### Client API  ###
+  ###             ###
+  ###             ###
+
+  @doc """
+  Bitcoin.Node.start_link
+
+  Starts a bitcoin full node
+  """
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts)
   end
 
+  @doc """
+  Bitcoin.Node.sync
+
+  Sync blocks with peers
+  """
   def sync(node) do
     GenServer.cast(node, {:sync})
   end
 
+  ###                      ###
+  ###                      ###
+  ### GenServer Callbacks  ###
+  ###                      ###
+  ###                      ###
+
+  @doc """
+  Bitcoin.Node.init
+
+  Initialize with ip_addr and seed process
+  """
+  @impl true
   def init(opts) do
     ip_addr = Keyword.get(opts, :ip_addr)
     seed = Keyword.get(opts, :seed)
@@ -22,17 +53,31 @@ defmodule Bitcoin.Node do
     {:ok, [ip_addr: ip_addr, blockchain: blockchain, chord_api: chord_api]}
   end
 
+  @doc """
+  Bitcoin.Node.handle_cast for `:sync`
+
+  callback to handle sync of peers
+  """
+  @impl true
   def handle_cast({:sync}, state) do
     top_hash = Bitcoin.Blockchain.get_top_hash(state[:blockchain])
     Chord.send_peers(state[:chord_api], :getblocks, {self(), top_hash})
     {:noreply, state}
   end
 
+  @doc """
+  Bitcoin.Node.handle_cast for `:blockchain_handler`
+
+  Callback to delegate the tasks to blockchain
+  """
   def handle_info({:blockchain_handler, message, payload}, state) do
     send(state[:blockchain], {:handle_message, message, payload})
   end
 
-  ###
+  ##### PRIVATE FUNCTIONS ####
+
+  # genesis_block
+  # Get the genesis block to initialize the blockchain
   defp genesis_block do
     genesis_transaction = %Bitcoin.Schemas.Transaction{}
 
